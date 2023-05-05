961e276cc55d5820d29b979c4a7c93020860cb21
JackTheBlack <ascona.nicolas@gmail.com>
2023-05-05 16:51:13 -0300

    Update infinite scroll

--- src/infiniteScroll.jsx
+++ src/infiniteScroll.jsx
@@ -1,12 +1,20 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect,  } from 'react';
 import Card from './card';
 export const InfiniteScrollExample = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+
+  //  esto es para ir llamando las distinas paginas
+  // page se incremneta en 1 cada vez que hago una peticion GET
   const [page, setPage] = useState(1);
   const [three,setThree]=useState([]);
+
+
+  ///index para ir agregando las peliculas
   const [index,setIndex]=useState(1);
+  
+  
   const [scroolTop, setScrollTop]=useState(document.documentElement.scrollTop)
 
 
@@ -14,24 +22,28 @@ export const InfiniteScrollExample = () => {
 
 
   const setThreeMovies=(movies)=>{
-    let newItems=items.slice(index,index);
+    // la primera vez le paso por referencia las tres primeras peliculas (emepzando desde la segunda)
+
     if (three.length<1){
       setThree(movies.slice(index,index+3));
    
       
     }else{
       
-
+    //la sgunda vez que entra al largo del array "three" ser mayor a 1
+    //se agrega las 3 peliculas que siguen en el estado items  
      
       setThree( [...three,...items.slice(index,index+3)])
     }
-    console.log(three)
+    
     console.log("page: ",page," response: ",movies)
     setIndex(index+3);
+    //incremento el index en 3
 
   }
 
-  // ... rest of the component
+
+
     const fetchData = async () => {
   setIsLoading(true);
   setError(null); 
@@ -48,22 +60,14 @@ export const InfiniteScrollExample = () => {
                    ))
   .catch(err => (console.log(err),setIsLoading(false)));
   
- /**  try {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=0b594805a17dec4e8de3882c93646258&language=en-US&page=1`);
-    const data = await response.json();
-console.log(response)
-  //  setItems(prevItems => [...prevItems, ...response.json()]);
-    setPage(prevPage => prevPage + 1);
-  } catch (error) {
-    setError(error);
-  } finally {
-    setIsLoading(false);
-  }*/
+
 };
 
 
 
 const handleScroll = () => {
+  //si la diferencia entre el scroll to anterior y el actuas es mayor a 56 entonces vuelvo
+  // hACER UN LLAMADO A la proxima pagina
   if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight || isLoading) {
    console.log("inner hight: ",window.innerHeight)
    console.log("old scroll top: ",scroolTop)
@@ -79,12 +83,16 @@ const handleScroll = () => {
 
 
 };
+// hook de efecto que se ejecuta una sola vez para cargar
+// las primeras 3 peliculas
 
 useEffect(() => {
  fetchData();
 },[]);
 
 
+
+// controla el cambio en el scroll
 useEffect(() => {
   window.addEventListener('scroll', handleScroll);
   return () => window.removeEventListener('scroll', handleScroll);
